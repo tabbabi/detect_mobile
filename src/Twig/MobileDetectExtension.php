a273cdfc9ca58cf82dd7e50f5ed374f8c785394b
mtabbabi <marwen.tabbabi@gmail.com>
2016-11-06 20:33:07 +0100

    Add twig render

--- src/Twig/MobileDetectExtension.php
+++ src/Twig/MobileDetectExtension.php
@@ -3,20 +3,25 @@ namespace Drupal\detect_mobile\Twig;
 
 use Drupal\detect_mobile\Detect\MobileDetect;
 use Drupal\Core\Template\TwigExtension;
+use Drupal\Core\Render\Renderer;
 /**
  * MobileDetectExtension class
  *
  * @author <marwen.tabbabi@gmail.com>
  */
-class MobileDetectExtension  extends TwigExtension{
-    
+class MobileDetectExtension  extends TwigExtension
+{
+    /**
+     * @var MobileDetect 
+     */
     protected $mobileDetector;
+    
     /**
      * Constructor
-     *
      */
-    public function __construct(MobileDetect $mobileDetector)
+    public function __construct(Renderer $render, MobileDetect $mobileDetector)
     {
+        parent::__construct($render);
         $this->mobileDetector = $mobileDetector;
     }
     
